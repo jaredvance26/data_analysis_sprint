e187f2214feabbeaff6390bebcc42e77bc8e191f
jaredvance26 <jaredkvance@gmail.com>
2021-12-01 19:38:22 -0700

    finished README

---
+++ README.md
@@ -0,0 +1,39 @@
+# Overview
+
+
+
+In this project, I am analyzing the marriages and divorces from 2015 - 2020. All of the data comes from 
+New Zealand. I wanted to see how marriage and divorce has been affected throughout the years as well as 
+the impact of COVID-19. 
+
+I wrote this software to learn more about data analysis. I know I scraped just the surface of it and but had
+a lot of fun with this project. 
+
+
+[Software Demo Video](https://www.youtube.com/watch?v=6U1KGDBrgbk)
+
+# Data Analysis Results
+
+<strong>Question:</strong> What has been the trend of marriage and divorce from the last 5 years or so?
+<strong>Answer:</strong> It has steadily declined. 
+
+
+<strong>Question:</strong> What was the affect of COVID-19 on marriage?
+<strong>Answer:</strong> It shot down considerably
+
+# Development Environment
+
+* Python 3.8
+* Pandas
+* Visual Studio Code
+
+
+
+# Useful Websites
+
+* [W3Schools](https://www.w3schools.com/)
+
+# Future Work
+
+* Finish analyzing median age data
+* Learn more and apply graphs with two sets of data
--- main.py
+++ main.py
@@ -5,38 +5,87 @@ import matplotlib.pyplot as pyplot
 median_marriage_age = pd.read_csv("marriage:divorce_data/median_marriage_age.csv")
 median_divorce_age = pd.read_csv("marriage:divorce_data/median_divorce_age.csv")
 marriage_divorce_count = pd.read_csv("marriage:divorce_data/marriage_divorce_count.csv")
-
+marriage_divorce_rate = pd.read_csv("marriage:divorce_data/marriage_divorce_rate.csv")
 
 def main():
-    count_graph()
+    
+    
+    valid = False
+    while not valid:
+        print()
+        choose = int(input("Select a graph(s) to view:\n1) Marriage and Divorce Count\n2) Marriage and Divorce Rate\n3) Median Marriage and Divorce Rate\n"))
+        
+
+        if choose == 1:
+            count_graph()
+            valid = True
+        elif choose == 2:
+            rate_graph()
+            valid = True
+        elif choose == 3:
+            median_married()
+            valid = True
+        else:
+            print("invalid answer")
+            valid = False
+
 
 def count_graph():
+    # Filter for marriage/divorce count to seperate the marriages from divorces
+    marriage_filter = (marriage_divorce_count["Marriages_or_Divorces"] == "Marriages")
+    divorce_filter = (marriage_divorce_count["Marriages_or_Divorces"] == "Divorces")
+
+    # Apply the filter to the data and plot the graphs
+    filtered_married = marriage_divorce_count[marriage_filter]
+    lineplot = filtered_married.plot(kind="line", x = "Period", y = "Count", title = "Marriage Count", color = "#004CFF")
+
+    filtered_divorced = marriage_divorce_count[divorce_filter]
+    lineplot = filtered_divorced.plot(kind="line", x = "Period", y = "Count", title = "Divorce Count", color = "#FF0000")
     
-    marriage_plot = marriage_count_plot()
-    divorce_plot = divorce_count_plot()
-    
- 
+    pyplot.tight_layout()
+    pyplot.show()
+
+
+def rate_graph():
+    # Filter for marriage/divorce rate to seperate the marriages from divorces
+    marriage_filter = (marriage_divorce_rate["General Marriage_Rate_and Divorce_Rate"] == "Marriage Rate")
+    divorce_filter = (marriage_divorce_rate["General Marriage_Rate_and Divorce_Rate"] == "Divorce Rate")
+
+    # Apply filter and then plot the graphs
+    filtered_married = marriage_divorce_rate[marriage_filter]
+    barplot = filtered_married.plot(kind = "bar", x = "Period", y = "Count", title = "Marriage Rate")
+
+    filtered_divorced = marriage_divorce_rate[divorce_filter]
+    barplot = filtered_divorced.plot(kind = "bar", x = "Period", y = "Count", title = "Divorce Rate", color = "red")
 
     pyplot.tight_layout()
     pyplot.show()
-         
-def marriage_count_plot():
-    marriage_filter = (marriage_divorce_count["Marriages_or_Divorces"] == "Marriages")
-    filtered_married = marriage_divorce_count[marriage_filter]
-    barplot = filtered_married.plot(kind="line", x = "Period", y = "Count", title = "Marriage Count", color = "#004CFF")
 
-    return barplot
-
-
-
-def divorce_count_plot():
-    divorce_filter = (marriage_divorce_count["Marriages_or_Divorces"] == "Divorces")
-    filtered_divorced = marriage_divorce_count[divorce_filter]
-    barplot = filtered_divorced.plot(kind="line", x = "Period", y = "Count", title = "Divorce Count", color = "#FF0000")
+def median_married():
     
-    return barplot
+    male_marriage_filter = (median_marriage_age['Male_or_Female'] == 'Male')
+    male_marriage_filter = (median_marriage_age[male_marriage_filter]["Period"] == 2010)
 
- 
+    # female_marriage_filter = (median_marriage_age['Male_or_Female'] == 'Female' and median_marriage_age["Status_before_Marriage"] == "Total")
+    
+   
+
+    line_plot = male_marriage_filter.plot(kind = "bar", x = "Status_before_Marriage", y = "Median_Age_at_Marriage")
+    # line_plot = female_marriage_filter.plot(kind = "line", x = "Period", y = "Median_Age_at_Marriage")
+   
+    pyplot.tight_layout()
+    pyplot.show()
+    
+   
+
+def median_divorce():
+    male_divorce_filter = (median_marriage_age['Male_or_Female'] == 'Male')
+    female_divorce_filter = (median_marriage_age['Male_or_Female'] == 'Female')
+     
+   
+    pyplot.tight_layout()
+    pyplot.show()
+
 main()
 
 
